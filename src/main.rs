a4490beac46a6128877e66d1f87d4cdb0d9f08df
Matt <mattkbrat@pm.me>
2024-01-13 18:42:31 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+slint::include_modules!();
+
+fn main() -> Result<(), slint::PlatformError> {
+    let ui = AppWindow::new()?;
+
+    let ui_handle = ui.as_weak();
+    ui.on_request_increase_value(move || {
+        let ui = ui_handle.unwrap();
+        ui.set_clicks(ui.get_clicks() + 1);
+    });
+
+    ui.run()
+}
