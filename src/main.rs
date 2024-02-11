d3eb7be9931e2d67b4a0c9d93b997b435b0b5350
Matthew Bratrsovsky <realzing@pm.me>
2024-02-10 17:25:10 -0700

    [style] increase default page size, button styles

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@
 
 use dioxus::html::geometry::euclid::num::Floor;
 use dioxus::prelude::*;
+use dioxus_desktop::{LogicalSize, WindowBuilder};
 use dioxus_router::prelude::*;
-
 mod lib;
 mod tests;
 mod client;
@@ -14,8 +14,18 @@ mod client;
 
 
 fn main() {
+
+    let window = WindowBuilder::new()
+        .with_title("AutoFLP")
+        .with_resizable(true)
+        .with_inner_size(LogicalSize::new(1000.0, 800.0))
+        .with_min_inner_size(LogicalSize::new(700.0, 700.0));
+
     dioxus_desktop::launch_cfg(
         client::App,
-        dioxus_desktop::Config::new().with_custom_head(r#"<link rel="stylesheet" href="dist/styles.css">"#.to_string()),
+        dioxus_desktop::Config::new()
+            .with_custom_head(r#"<link rel="stylesheet" href="dist/styles.css">"#.to_string())
+            .with_window(window)
+        ,
     )
 }
