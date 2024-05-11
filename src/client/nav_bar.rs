b2511bc7dcf5486286cc02f1ab79ed9791a2cecc
Matthew Bratrsovsky <git.twisted254@slmail.me>
2024-05-11 12:13:34 -0600

    fix: format

--- src/client/nav_bar.rs
+++ src/client/nav_bar.rs
@@ -1,10 +1,10 @@
+use crate::client;
 use dioxus::prelude::*;
 use dioxus_router::prelude::*;
-use crate::client;
 
 use crate::client::{PeopleNamesVec, Route};
 use crate::lib::account::get_full_name::{full_name_from_person, FullNameFormat};
-use crate::lib::database::account::get_account_people::{AccountPeople, get_account_people};
+use crate::lib::database::account::get_account_people::{get_account_people, AccountPeople};
 use crate::lib::database::models::PersonName;
 use crate::lib::qotd::Quotable;
 use crate::lib::unsplash::structs::UnsplashResults;
@@ -20,23 +20,30 @@ pub fn NavBar(cx: Scope) -> Element {
             if names.read().is_empty() {
                 let these_people = match people.read().is_empty() {
                     true => get_account_people(),
-                    false => people.read().clone()
+                    false => people.read().clone(),
                 };
 
-                let these_names = these_people.iter().map(|x| {
-                    let last = (&x.0).to_string();
-                    let first = (&x.1).to_string();
+                let these_names = these_people
+                    .iter()
+                    .map(|x| {
+                        let last = x.0.to_string();
+                        let first = x.1.to_string();
 
-                    let full_name = full_name_from_person(&PersonName {
-                        first_name: first,
-                        last_name: last,
-                        middle_initial: None,
-                        name_prefix: None,
-                        name_suffix: None,
-                    }, FullNameFormat::LastFirstMiddleSuffix, true);
+                        let full_name = full_name_from_person(
+                            &PersonName {
+                                first_name: first,
+                                last_name: last,
+                                middle_initial: None,
+                                name_prefix: None,
+                                name_suffix: None,
+                            },
+                            FullNameFormat::LastFirstMiddleSuffix,
+                            true,
+                        );
 
-                    [full_name, (&x.2).to_string()]
-                }).collect::<PeopleNamesVec>();
+                        [full_name, x.2.to_string()]
+                    })
+                    .collect::<PeopleNamesVec>();
 
                 *names.write() = these_names;
                 if people.read().is_empty() {
